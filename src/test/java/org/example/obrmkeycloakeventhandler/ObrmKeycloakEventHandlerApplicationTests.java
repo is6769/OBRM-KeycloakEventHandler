9cefe9746cf6f54b3ef61638b21b494eb9563469
is6769 <igor.shvets2007@mail.ru>
2025-03-28 17:32:01 +0300

    first commit

---
+++ src/test/java/org/example/obrmkeycloakeventhandler/ObrmKeycloakEventHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.obrmkeycloakeventhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObrmKeycloakEventHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
